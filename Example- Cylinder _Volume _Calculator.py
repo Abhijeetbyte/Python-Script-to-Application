1ac188c11f2b369a235a2cade5ebf1993d811b4d
4BHIJEET <80936610+4BH1J337@users.noreply.github.com>
2021-05-26 12:24:48 +0530

    Add files via upload

---
+++ Example- Cylinder _Volume _Calculator.py
@@ -0,0 +1,31 @@
+print("                               _____________                                           ")
+print("                              /             \                                          ")
+print("                             |\_____________/|                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             |               |                                         ")
+print("                             | _____________ |                                         ")
+print("                             |/             \|                                         ")
+print("                              \_____________/                                          ")
+print(2 * '\n') #Three Blank lines
+print("                        Volume of Cylinder Calculator")
+print(2 * '\n')                                                                                  
+print("                'Enter your Values below, then press enter for next'")
+print(2 * '\n')
+
+H= float(input("                        >>> Enter Height in cm =  "))
+R= float(input("                        >>> Enter  Radius  in cm =  "))
+PI=22/7*R*R*H
+
+print(3 * '\n') #Four Blank lines
+print("                      Here is the Volume of Cylinder ")
+print(1 * '\n') #Two Blank lines
+print("                               >>>",round(PI,2),"Cm3")
+print(2 * '\n')
+
+close=str(input("                -----------PRESS ENTER TO CLOSE ---------"))
+    
