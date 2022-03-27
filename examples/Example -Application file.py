242578ddbfe5c0dfa2bffb4a1f4a7d1bd0e07f9c
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-03-27 16:38:04 +0530

    Example -script file

---
+++ examples/Example -Application file.py
@@ -0,0 +1,19 @@
+ #This is an example Python script for calculating the volume of a right angle cylinder                
+print(2 * '\n') #lank lines
+print("                        Volume of Cylinder Calculator")
+print(2 * '\n')                                                                                  
+print("                'Enter your Values below, then press enter for next'")
+print(2 * '\n')
+
+H= float(input("                        >>> Enter Height in cm =  "))
+R= float(input("                        >>> Enter  Radius  in cm =  "))
+PI=22/7*R*R*H
+
+print(3 * '\n') # Blank lines
+print("                      Here is the Volume of Cylinder ")
+print(1 * '\n') #Blank lines
+print("                               >>>",round(PI,2),"Cm3")
+print(2 * '\n')
+
+close=str(input("                -----------PRESS ENTER TO CLOSE ---------"))
+    
